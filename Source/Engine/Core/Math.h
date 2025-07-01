24a7069c948313fa187a3d36d761b03f52f2c9b0
Raymond L Maple <rmaple@neumont.edu>
2025-07-01 10:16:25 -0600

    add core files

---
+++ Source/Engine/Core/Math.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace viper {
+	constexpr float pi = 3.1415926535897932384626433832795f;
+	constexpr float twoPi = 2 * pi;
+	constexpr float halfPi = pi * 0.5f;
+}
--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -1,6 +1,11 @@
+#include "../Engine/Core/Math.h"
 #include <iostream>
+
+#define NAME "Raymond\n"
 
 int main()
 {
+	std::cout << NAME;
 	std::cout << "Hello, World!\n";
+	std::cout << viper::pi << std::endl;
 }
